59b9c3985cbd465510f4089bb9214abce7bf8356
mrbernnz <noel.sagaille@gmail.com>
2017-11-14 10:14:16 -0500

    * Configure SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,18 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'app/controllers/'
+  add_filter 'app/channels/'
+  add_filter 'app/jobs/'
+  add_filter 'app/mailers/'
+  SimpleCov.minimum_coverage 90
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+
   fixtures :all
 
   Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
