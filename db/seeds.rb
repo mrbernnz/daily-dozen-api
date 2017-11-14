196de8026906a55313416456470d354895158855
mrbernnz <noel.sagaille@gmail.com>
2017-11-14 10:14:50 -0500

    * Edit seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 athlete = Athlete.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  email: Faker::Internet.email,
-  password: Faker::Internet.password(8)
 )
 
 WORKOUTS = ['Chest and Biceps', 'Back and Triceps', 'Legs and Shoulders']
