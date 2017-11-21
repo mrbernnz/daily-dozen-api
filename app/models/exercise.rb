95a5d6b5e51bd30aad356bbd04ec17342ba4fc8f
mrbernnz <noel.sagaille@gmail.com>
2017-11-21 12:08:26 -0500

    * Update models
      Why:
    
        Add validators

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,6 @@
 class Exercise < ApplicationRecord
   belongs_to :workout
   has_many :exercise_sets
+
+  validates_presence_of :exercise_name, :workout_id
 end
--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,4 +1,6 @@
 class ExerciseSet < ApplicationRecord
   self.table_name = 'sets'
   belongs_to :exercise
+
+  validates_presence_of :number, :weight, :unit, :rep, :exercise_id
 end
