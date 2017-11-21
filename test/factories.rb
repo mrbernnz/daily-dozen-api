2fbf9fb5147998aba22c2ff7b0a8f9c3ac7ae193
mrbernnz <noel.sagaille@gmail.com>
2017-11-21 12:09:25 -0500

    * Update factory file.
      Why:
    
        To mirror changes in migrations

--- test/factories.rb
+++ test/factories.rb
@@ -5,24 +5,14 @@ FactoryBot.define do
     email Faker::Internet.email
     password Faker::Internet.password
     token Faker::Crypto.sha1
-
-    factory :athlete_with_workouts do
-      transient do
-        workouts_count 5
-      end
-
-      after(:build) do |athlete, evaluator|
-        create_list(:workout, evaluator.workouts_count, athlete: athlete)
-      end
-    end
   end
 
   factory :workout do
     workout_name 'Chest and Biceps'
-    date 1.day.ago
-    start_time 2.hours.ago
-    end_time 10.minutes.ago
-    notes 'Great workout today!'
+    day 1.day.ago
+    start 2.hours.ago
+    finish 10.minutes.ago
+    notes Faker::Lorem.sentence
     athlete
   end
 
