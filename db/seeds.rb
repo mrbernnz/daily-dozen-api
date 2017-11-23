5a8ba12a12134ff893c96e22270ddb42b4edaf1b
mrbernnz <noel.sagaille@gmail.com>
2017-11-22 21:39:49 -0500

    * Update seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ athlete = Athlete.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   email: Faker::Internet.email,
-  password: 'password'
+  password: 'password',
+  token: Faker::Crypto.sha1
 )
 
 WORKOUTS = ['Chest and Biceps', 'Back and Triceps', 'Legs and Shoulders']
@@ -11,9 +12,9 @@ EXERCISES = ['INCLINE DUMBBELL PRESS', 'INCLINE DUMBBELL FLY', 'DUMBBELL BENCH P
 WORKOUTS.each do |workout|
   athlete.workouts.create(
     workout_name: workout,
-    date: Faker::Date.backward(2),
-    start_time: Faker::Time.between(2.hours.ago, DateTime.now),
-    end_time: Faker::Time.between(5.minutes.ago, DateTime.now),
+    day: Faker::Date.backward(2),
+    start: Faker::Time.between(2.hours.ago, DateTime.now),
+    finish: Faker::Time.between(5.minutes.ago, DateTime.now),
     notes: Faker::Lorem.sentence(4)
   )
 end
